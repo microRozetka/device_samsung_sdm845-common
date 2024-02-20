bb29bfda360d336c3a6e5af53c9e0cb6b8e91dc2
Unkonow <you@example.com>
2024-02-20 09:43:46 +0900

    sdm845-common: Remove virtual framebuffer  * It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.
    Change-Id: I51becacd30d17e71cdeb0fbe31bbe1b94262b316
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,7 +72,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET := 0x02000000
 
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 $
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image/ loop.max_part=7
