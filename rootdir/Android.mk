070fbbc98fc9a329731525bea15b5b7c2ae456f3
microRozetka <ilyapokidow@gmail.com>
2024-04-21 19:29:10 +0000

    sdm845-common: fix LOCAL_SRC_FILES of init.recovery.sdm845.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := init.recovery.sdm845.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.recovery.sdm845.rc
+LOCAL_SRC_FILES         := etc/init.recovery.sdm845.rc
 LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
